5a7d278a3e41bbab45369c5ab3aa0ae1d1d05868
infolock <jhibbard@gmail.com>
2011-05-30 16:24:10 -0400

    Updated REST for test

---
+++ REST/src/Controllers/v1.php
@@ -0,0 +1,11 @@
+<?php
+namespace REST;
+class v1 implements \Lists\IController {
+    public function __construct() {
+    }
+
+    public function execute($request) {
+        echo "hey bitch!";
+    }
+}
+?>
