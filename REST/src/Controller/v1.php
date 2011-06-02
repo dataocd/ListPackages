9c643840cf050dd04f085c42497c156eff1f2c03
JamesWPhillips <james@jameswphillips.com>
2011-06-02 12:52:43 -0400

    Updated to demo some REST tests

--- REST/src/Controller/v1.php
+++ REST/src/Controller/v1.php
@@ -6,8 +6,26 @@ class v1 implements \Lists\Controller\IController {
 
     public function execute($request) {
         echo $request->packageCommand.'<BR>';
-        echo $request->get('var');
-        echo 'This is the command to perform';
+        //echo $request->get('var').'<BR>';
+        $request_type = strtolower($request->get('REQUEST_METHOD'));
+        switch ($request_type) {
+            case 'get':
+                echo 'Its a get request: <BR>';
+                print_r($_GET);
+                break;
+            case 'post':
+                echo 'Its a post request: <BR>';
+                print_r($_POST);
+                break;
+            case 'put':
+                echo 'Its a put request: <BR>';
+                print_r($_PUT);
+                break;
+            default:
+                echo 'Something went wrong... throw here';
+                break;
+        }
+        
     }
 }
 ?>
